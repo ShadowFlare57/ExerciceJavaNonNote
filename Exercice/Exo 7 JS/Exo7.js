00bb50ce48295a16a50af0494e553040c86d03da
ShadowFlare57 <shadow57100@gmail.com>
2021-02-02 09:23:59 +0100

    board with stock done

--- Exercice/Exo 7 JS/Exo7.js
+++ Exercice/Exo 7 JS/Exo7.js
@@ -1,33 +1,32 @@
+ /* Déclaration de variable avant la fonction pour la création de tableau */
+
+var stock = 0;
+var produit = [];
+
+
 function result()
 {
-    var i;
+    var i
     var noms = document.getElementById('nom').value;
     var nbr = document.getElementById('nombre').value;
     var thune = document.getElementById('prix').value;
-    var textnom = '';
-    var textnbr ='';
-    var textprix = '';
+    var text = '';
 
-    if(noms !='') {
-        document.getElementById('ErreurNom').style.display="none";
+    /* Ajout des noms des produits et de la quantité */
 
+    produit.push(noms);
+    stock = stock + (nbr*thune);
+    produit.sort();
+
+    for (i=0; i<produit.length; i++) {
+        text+= produit[i]+ '<br>'
     }
-    else
-    {
-        document.getElementById('Erreurnom').style.display="inline-block";
-    }
-    if (nbr !='') {
-        document.getElementById('ErreurNbr').style.display="none";
-    }
-    else
-    {
-        document.getElementById('ErreurNbr').style.display="inline-block";
-    }
-    if (thune !='') {
-        document.getElementById('ErreurPrix').style.display="none";
-    }
-    else
-    {
-        document.getElementById('ErreurPrix').style.display="inline-block";
-    }
+    /* Récupération des valeurs des variables dans le HTML */
+    
+    document.getElementById('nomproduit').innerHTML = 'Nom des produits en stock : <br>' +text;
+    document.getElementById('totalstock').innerHTML = 'Total du prix dans le stock = ' +stock+ ' €';
+    document.getElementById('nom').value='';
+    document.getElementById('quantite').value='';
+    document.getElementById('prix').value='';
+
 }
